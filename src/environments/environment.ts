d324fa643804e1b72b2214e52ae50943face36f0
Basdlwaschdl <ftpobi@googlemail.com>
2023-06-22 19:46:29 +0200

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 export const environment = {
   firebase: {
     projectId: 'ring-of-fire-987eb',
-    appId: '1:511014371741:web:7f41f3c89e9113bb866f74',
+    appId: '1:511014371741:web:036ef68ef74cd54c866f74',
+    databaseURL: 'https://ring-of-fire-987eb-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ring-of-fire-987eb.appspot.com',
+    locationId: 'europe-west',
     apiKey: 'AIzaSyC-8oNhU26IiIsD8qQ8nCpBWzf-tcBrmos',
     authDomain: 'ring-of-fire-987eb.firebaseapp.com',
     messagingSenderId: '511014371741',
